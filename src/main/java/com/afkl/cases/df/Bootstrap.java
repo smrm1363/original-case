ffc2d84ee60f85b93b6eb1c7c57cf23cef814eec
Mohamadreza Mirali <smrm1363@gmail.com>
2019-02-10 18:32:29 +0330

    near to final

--- src/main/java/com/afkl/cases/df/Bootstrap.java
+++ src/main/java/com/afkl/cases/df/Bootstrap.java
@@ -2,6 +2,10 @@ package com.afkl.cases.df;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.io.IOException;
 
@@ -11,5 +15,15 @@ public class Bootstrap {
     public static void main(String[] args) throws IOException {
         SpringApplication.run(Bootstrap.class, args);
     }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+                        .allowedHeaders("*");
+            }
+        };
 
+}
 }
